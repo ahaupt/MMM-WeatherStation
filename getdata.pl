db840054cca9edd093c69c19291f1f24a0847112
Andreas Haupt <andreas.haupt@desy.de>
2023-05-20 15:55:10 +0200

    Feinstaub min/max

--- getdata.pl
+++ getdata.pl
@@ -21,35 +21,35 @@ $ua = LWP::UserAgent->new();
 $ua->timeout(1.0);
 $response = $ua->request($request);
 if ( $response->is_success() ) {
-	$data = from_json($response->content(), { utf8 => 1 });
+    $data = from_json($response->content(), { utf8 => 1 });
 }
 $request = HTTP::Request->new('GET' => $URL2 );
 $response = $ua->request($request);
 if ( $response->is_success() ) {
-	my $data2 = from_json($response->content(), { utf8 => 1 });
-	# in case first sensor is dead or is more exposed to sunlight
-	if ( ! defined $data->{'outdoor-temperature'} or $data2->{'current'}{'outdoor_temperature'} < $data->{'outdoor-temperature'} ) {
-		$data->{'outdoor-temperature'} = $data2->{'current'}{'outdoor_temperature'};
-		$data->{'outdoor-humidity'} = $data2->{'current'}{'outdoor_humidity'};
-	}
-	$data->{'air-pressure'} =  $data2->{'current'}{'air_pressure'};
-	$data->{'outdoor-pm2.5'} = $data2->{'current'}{'pm2_5'};
-	$data->{'outdoor-pm10'} = $data2->{'current'}{'pm10'};
+    my $data2 = from_json($response->content(), { utf8 => 1 });
+    # in case first sensor is dead or is more exposed to sunlight
+    if ( ! defined $data->{'outdoor-temperature'} or $data2->{'current'}{'outdoor_temperature'} < $data->{'outdoor-temperature'} ) {
+	$data->{'outdoor-temperature'} = $data2->{'current'}{'outdoor_temperature'};
+	$data->{'outdoor-humidity'} = $data2->{'current'}{'outdoor_humidity'};
+    }
+    $data->{'air-pressure'} =  $data2->{'current'}{'air_pressure'};
+    $data->{'outdoor-pm2.5'} = $data2->{'current'}{'pm2_5'};
+    $data->{'outdoor-pm10'} = $data2->{'current'}{'pm10'};
 }
 
 if ( open LOCAL_DATA, "/dev/shm/indoor-data" ) {
-	chomp($_ = <LOCAL_DATA>);
-	my ($temp, $humi, $pressure) = split /\s+/;
-	close LOCAL_DATA;
-	$data->{'indoor-temperature'} = $temp;
-	$data->{'indoor-humidity'} = $humi;
-	$data->{'air-pressure'} = $pressure;
+    chomp($_ = <LOCAL_DATA>);
+    my ($temp, $humi, $pressure) = split /\s+/;
+    close LOCAL_DATA;
+    $data->{'indoor-temperature'} = $temp;
+    $data->{'indoor-humidity'} = $humi;
+    $data->{'air-pressure'} = $pressure;
 }
 
 if ( open LOCAL_DATA, "/dev/shm/indoor-data-co2" ) {
-	chomp($_ = <LOCAL_DATA>);
-	close LOCAL_DATA;
-	$data->{'indoor-co2'} = $_;
+    chomp($_ = <LOCAL_DATA>);
+    close LOCAL_DATA;
+    $data->{'indoor-co2'} = $_;
 }
 
 my $dbh = sqlite_db();
@@ -57,45 +57,49 @@ safe_data($dbh, $data);
 my $minmax = get_minmax($dbh);
 
 $output = {
-	'air-pressure'		=> $data->{'air-pressure'},
-	'air-pressure-1hour'	=> $minmax->{'air-pressure-1hour'} || $data->{'air-pressure'},
-	'indoor-co2'		=> $data->{'indoor-co2'},
-	'indoor-co2-min'	=> $minmax->{'indoor-co2-min'},
-	'indoor-co2-max'	=> $minmax->{'indoor-co2-max'},
-	'indoor-humidity'	=> $data->{'indoor-humidity'},
-	'indoor-humidity-min'	=> $minmax->{'indoor-humi-min'},
-	'indoor-humidity-max'	=> $minmax->{'indoor-humi-max'},
-	'indoor-celcius'	=> $data->{'indoor-temperature'},
-	'indoor-fahrenheit'	=> to_fahrenheit($data->{'indoor-temperature'}),
-	'indoor-kelvin'		=> to_kelvin($data->{'indoor-temperature'}),
-	'indoor-celcius-min'	=> $minmax->{'indoor-temp-min'},
-	'indoor-fahrenheit-min'	=> to_fahrenheit($minmax->{'indoor-temp-min'}),
-	'indoor-kelvin-min'	=> to_kelvin($minmax->{'indoor-temp-min'}),
-	'indoor-celcius-max'	=> $minmax->{'indoor-temp-max'},
-	'indoor-fahrenheit-max'	=> to_fahrenheit($minmax->{'indoor-temp-max'}),
-	'indoor-kelvin-max'	=> to_kelvin($minmax->{'indoor-temp-max'}),
-	'indoor-dewpoint-celcius'=> dewpoint($data->{'indoor-temperature'}, $data->{'indoor-humidity'}),
-	'outdoor-pm2.5'		=> $data->{'outdoor-pm2.5'},
-	'outdoor-pm10'		=> $data->{'outdoor-pm10'},
-	'outdoor-humidity'	=> $data->{'outdoor-humidity'},
-	'outdoor-humidity-min'	=> $minmax->{'outdoor-humi-min'},
-	'outdoor-humidity-max'	=> $minmax->{'outdoor-humi-max'},
-	'outdoor-celcius'	=> $data->{'outdoor-temperature'},
-	'outdoor-fahrenheit'	=> to_fahrenheit($data->{'outdoor-temperature'}),
-	'outdoor-kelvin'	=> to_kelvin($data->{'outdoor-temperature'}),
-	'outdoor-celcius-min'	=> $minmax->{'outdoor-temp-min'},
-	'outdoor-fahrenheit-min'=> to_fahrenheit($minmax->{'outdoor-temp-min'}),
-	'outdoor-kelvin-min'	=> to_kelvin($minmax->{'outdoor-temp-min'}),
-	'outdoor-celcius-max'	=> $minmax->{'outdoor-temp-max'},
-	'outdoor-fahrenheit-max'=> to_fahrenheit($minmax->{'outdoor-temp-max'}),
-	'outdoor-kelvin-max'	=> to_kelvin($minmax->{'outdoor-temp-max'}),
-	'outdoor-dewpoint-celcius'=> dewpoint($data->{'outdoor-temperature'}, $data->{'outdoor-humidity'}),
+    'air-pressure'		=> $data->{'air-pressure'},
+    'air-pressure-1hour'	=> $minmax->{'air-pressure-1hour'} || $data->{'air-pressure'},
+    'indoor-co2'		=> $data->{'indoor-co2'},
+    'indoor-co2-min'		=> $minmax->{'indoor-co2-min'},
+    'indoor-co2-max'		=> $minmax->{'indoor-co2-max'},
+    'indoor-humidity'		=> $data->{'indoor-humidity'},
+    'indoor-humidity-min'	=> $minmax->{'indoor-humi-min'},
+    'indoor-humidity-max'	=> $minmax->{'indoor-humi-max'},
+    'indoor-celcius'		=> $data->{'indoor-temperature'},
+    'indoor-fahrenheit'		=> to_fahrenheit($data->{'indoor-temperature'}),
+    'indoor-kelvin'		=> to_kelvin($data->{'indoor-temperature'}),
+    'indoor-celcius-min'	=> $minmax->{'indoor-temp-min'},
+    'indoor-fahrenheit-min'	=> to_fahrenheit($minmax->{'indoor-temp-min'}),
+    'indoor-kelvin-min'		=> to_kelvin($minmax->{'indoor-temp-min'}),
+    'indoor-celcius-max'	=> $minmax->{'indoor-temp-max'},
+    'indoor-fahrenheit-max'	=> to_fahrenheit($minmax->{'indoor-temp-max'}),
+    'indoor-kelvin-max'		=> to_kelvin($minmax->{'indoor-temp-max'}),
+    'indoor-dewpoint-celcius'	=> dewpoint($data->{'indoor-temperature'}, $data->{'indoor-humidity'}),
+    'outdoor-pm2.5'		=> $data->{'outdoor-pm2.5'},
+    'outdoor-pm2.5-min'		=> $minmax->{'outdoor-pm2.5-min'},
+    'outdoor-pm2.5-max'		=> $minmax->{'outdoor-pm2.5-max'},
+    'outdoor-pm10'		=> $data->{'outdoor-pm10'},
+    'outdoor-pm10-min'		=> $minmax->{'outdoor-pm10-min'},
+    'outdoor-pm10-max'		=> $minmax->{'outdoor-pm10-max'},
+    'outdoor-humidity'		=> $data->{'outdoor-humidity'},
+    'outdoor-humidity-min'	=> $minmax->{'outdoor-humi-min'},
+    'outdoor-humidity-max'	=> $minmax->{'outdoor-humi-max'},
+    'outdoor-celcius'		=> $data->{'outdoor-temperature'},
+    'outdoor-fahrenheit'	=> to_fahrenheit($data->{'outdoor-temperature'}),
+    'outdoor-kelvin'		=> to_kelvin($data->{'outdoor-temperature'}),
+    'outdoor-celcius-min'	=> $minmax->{'outdoor-temp-min'},
+    'outdoor-fahrenheit-min'	=> to_fahrenheit($minmax->{'outdoor-temp-min'}),
+    'outdoor-kelvin-min'	=> to_kelvin($minmax->{'outdoor-temp-min'}),
+    'outdoor-celcius-max'	=> $minmax->{'outdoor-temp-max'},
+    'outdoor-fahrenheit-max'	=> to_fahrenheit($minmax->{'outdoor-temp-max'}),
+    'outdoor-kelvin-max'	=> to_kelvin($minmax->{'outdoor-temp-max'}),
+    'outdoor-dewpoint-celcius'	=> dewpoint($data->{'outdoor-temperature'}, $data->{'outdoor-humidity'}),
 };
 print encode_json($output), "\n";
 $lockmgr->unlock($LOCK_FILE);
 
 sub sqlite_db {
-    my $schema = "indoor_temp REAL, indoor_humi REAL, outdoor_temp REAL, outdoor_humi REAL, pressure REAL, co2 REAL, time DATETIME DEFAULT CURRENT_TIMESTAMP";
+    my $schema = "indoor_temp REAL, indoor_humi REAL, outdoor_temp REAL, outdoor_humi REAL, pressure REAL, co2 REAL, pm25 REAL, pm10 REAL, time DATETIME DEFAULT CURRENT_TIMESTAMP";
     my $dbh = DBI->connect("dbi:SQLite:dbname=$SQLITE_FILE","","");
     $dbh->do("CREATE TABLE IF NOT EXISTS $TABLE_NAME ($schema)")
 	or die $DBI::errstr;
@@ -104,14 +108,16 @@ sub sqlite_db {
 
 sub safe_data {
     my ($dbh, $data) = @_;
-    my $sth = $dbh->prepare("INSERT INTO $TABLE_NAME (indoor_temp, indoor_humi, outdoor_temp, outdoor_humi, pressure, co2) VALUES (?,?,?,?,?,?)");
+    my $sth = $dbh->prepare("INSERT INTO $TABLE_NAME (indoor_temp, indoor_humi, outdoor_temp, outdoor_humi, pressure, co2, pm25, pm10) VALUES (?,?,?,?,?,?,?,?)");
     $sth->execute(
 	$data->{'indoor-temperature'},
 	$data->{'indoor-humidity'},
 	$data->{'outdoor-temperature'},
 	$data->{'outdoor-humidity'},
 	$data->{'air-pressure'},
-	$data->{'indoor-co2'}
+	$data->{'indoor-co2'},
+	$data->{'outdoor-pm2.5'},
+	$data->{'outdoor-pm10'}
     );
 }
 
@@ -150,15 +156,19 @@ sub get_minmax {
     $sth->execute();
     $row = $sth->fetchrow_arrayref();
     $data->{"air-pressure-1hour"} = $row->[0];
+    foreach my $sensor ( qw(indoor-co2 outdoor-pm2.5 outdoor-pm10) ) {
+	$sensor =~ m|door\-(.+)$|;
+	my $short_name = $1; $short_name =~ s/\.//g;
 
-    $sth = $dbh->prepare("SELECT min(co2) FROM weatherdata WHERE time > datetime(datetime('now', '-1 days'))");
-    $sth->execute();
-    $row = $sth->fetchrow_arrayref();
-    $data->{"indoor-co2-min"} = $row->[0];
-    $sth = $dbh->prepare("SELECT max(co2) FROM weatherdata WHERE time > datetime(datetime('now', '-1 days'))");
-    $sth->execute();
-    $row = $sth->fetchrow_arrayref();
-    $data->{"indoor-co2-max"} = $row->[0];
+	$sth = $dbh->prepare("SELECT min($short_name) FROM weatherdata WHERE time > datetime(datetime('now', '-1 days'))");
+	$sth->execute();
+	$row = $sth->fetchrow_arrayref();
+	$data->{"$sensor-min"} = $row->[0];
+	$sth = $dbh->prepare("SELECT max($short_name) FROM weatherdata WHERE time > datetime(datetime('now', '-1 days'))");
+	$sth->execute();
+	$row = $sth->fetchrow_arrayref();
+	$data->{"$sensor-max"} = $row->[0];
+    }
 #    $d->dumpValue($data);
     return $data;
 }
